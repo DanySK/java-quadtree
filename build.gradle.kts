68bb243da760227bf4a15567fe4976618cb05890
Danilo Pianini <danilo.pianini@unibo.it>
2019-10-10 15:40:08 +0200

    Refresh build system

---
+++ build.gradle.kts
@@ -0,0 +1,84 @@
+import com.github.spotbugs.SpotBugsTask
+
+plugins {
+    `java-library`
+    checkstyle
+    pmd
+    signing
+    `maven-publish`
+    `project-report`
+    `build-dashboard`
+    jacoco
+    id("com.github.spotbugs") version Versions.com_github_spotbugs_gradle_plugin
+    id("de.fayard.buildSrcVersions") version Versions.de_fayard_buildsrcversions_gradle_plugin
+    id("org.danilopianini.git-sensitive-semantic-versioning") version Versions.org_danilopianini_git_sensitive_semantic_versioning_gradle_plugin
+    id("org.danilopianini.javadoc.io-linker") version Versions.org_danilopianini_javadoc_io_linker_gradle_plugin
+    id("org.danilopianini.publish-on-central") version Versions.org_danilopianini_publish_on_central_gradle_plugin
+    id("org.jlleitschuh.gradle.ktlint") version Versions.org_jlleitschuh_gradle_ktlint_gradle_plugin
+}
+
+gitSemVer {
+    version = computeGitSemVer()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(Libs.guava)
+    testImplementation("junit:junit:[4.12, 5[")
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.withType<SpotBugsTask> {
+    reports {
+        xml.setEnabled(false)
+        html.setEnabled(true)
+    }
+    ignoreFailures = false
+    effort = "max"
+    reportLevel = "low"
+    File("${project.rootProject.projectDir}/config/spotbugs/excludes.xml")
+        .takeIf { it.exists() }
+        ?.also { excludeFilterConfig = project.resources.text.fromFile(it) }
+        ?.also { println(it) }
+}
+
+pmd {
+    ruleSets = listOf()
+    ruleSetConfig = resources.text.fromFile("${project.rootProject.projectDir}/config/pmd/pmd.xml")
+}
+
+group = "org.danilopianini"
+publishOnCentral {
+    projectDescription.set("Java spatial indexing tools")
+    projectLongName.set("Java QuadTree")
+}
+
+if (System.getenv("CI") == true.toString()) {
+    signing {
+        val signingKey: String? by project
+        val signingPassword: String? by project
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                developers {
+                    developer {
+                        name.set("Danilo Pianini")
+                        email.set("danilo.pianini@gmail.com")
+                        url.set("http://www.danilopianini.org/")
+                    }
+                }
+            }
+        }
+    }
+}
--- travis_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-openssl aes-256-cbc -K $encrypted_ff8e67061518_key -iv $encrypted_ff8e67061518_iv -in prepare_environment.sh.enc -out prepare_environment.sh -d
-bash prepare_environment.sh
-./gradlew
-./gradlew uploadArchives
