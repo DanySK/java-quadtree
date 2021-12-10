501ed10be3de06661fe5592da9d53e2479bd743d
Danilo Pianini <danilo.pianini@unibo.it>
2021-12-10 14:00:48 +0100

    ci: switch to the new build system and ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,10 @@
 plugins {
     `java-library`
-    checkstyle
-    pmd
-    signing
-    `maven-publish`
-    `project-report`
-    `build-dashboard`
-    jacoco
-    id("com.github.spotbugs")
-    id("org.danilopianini.git-sensitive-semantic-versioning")
-    id("org.danilopianini.javadoc.io-linker")
-    id("org.danilopianini.publish-on-central")
-    id("org.jlleitschuh.gradle.ktlint")
+    alias(libs.plugins.gitSemVer)
+    alias(libs.plugins.java.qa)
+    alias(libs.plugins.multiJvmTesting)
+    alias(libs.plugins.publishOnCentral)
+    alias(libs.plugins.taskTree)
 }
 
 repositories {
@@ -19,43 +12,15 @@ repositories {
 }
 
 dependencies {
-    val spotbugsLibrary = "com.github.spotbugs:spotbugs:_"
-    implementation("com.google.guava:guava:_")
-    testCompileOnly(spotbugsLibrary)
-    testImplementation("junit:junit:_")
-    spotbugs(spotbugsLibrary)
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-}
-
-spotbugs {
-    setEffort("max")
-    setReportLevel("low")
-    showProgress.set(true)
-    val excludeFile = File("${project.rootProject.projectDir}/config/spotbugs/excludes.xml")
-    if (excludeFile.exists()) {
-        excludeFilter.set(excludeFile)
-    }
-}
-
-tasks.withType<com.github.spotbugs.snom.SpotBugsTask> {
-    reports {
-        create("html") { enabled = true }
-    }
-}
-
-pmd {
-    ruleSets = listOf()
-    ruleSetConfig = resources.text.fromFile("${project.rootProject.projectDir}/config/pmd/pmd.xml")
+    implementation(libs.guava)
+    testCompileOnly(libs.spotbugs.annotations)
+    testImplementation(libs.junit4)
 }
 
 group = "org.danilopianini"
 publishOnCentral {
-    projectDescription = "Java spatial indexing tools"
-    projectLongName = "Java QuadTree"
+    projectDescription.set("Java spatial indexing tools")
+    projectLongName.set("Java QuadTree")
 }
 
 if (System.getenv("CI") == true.toString()) {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,2 @@
-import org.danilopianini.VersionAliases.justAdditionalAliases
-
-plugins {
-    id("de.fayard.refreshVersions") version "0.10.1"
-}
-
-refreshVersions {
-    extraArtifactVersionKeyRules = justAdditionalAliases
-}
-
-buildscript {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.danilopianini:refreshversions-aliases:+")
-    }
-}
-
 rootProject.name = "java-quadtree"
+enableFeaturePreview("VERSION_CATALOGS")
