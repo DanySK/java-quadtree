9dab9e96d8458016bf631d10992a49702d163daa
Danilo Pianini <danilo.pianini@gmail.com>
2022-01-06 20:50:11 +0100

    chore(release): correctly enable semantic commit conventions

---
+++ release.config.js
@@ -0,0 +1,18 @@
+var publishCmd = `
+git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+git push --force origin \${nextRelease.version}
+./gradlew releaseJavaMavenOnMavenCentralNexus --parallel || exit 1
+`
+var config = require('semantic-release-preconfigured-conventional-commits');
+config.plugins.push(
+    ["@semantic-release/exec", {
+        "publishCmd": publishCmd,
+    }],
+    ["@semantic-release/github", {
+        "assets": [
+            { "path": "build/shadow/*-all.jar" },
+        ]
+    }],
+    "@semantic-release/git",
+)
+module.exports = config
