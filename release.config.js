1fe93223fa34232f1342d15be4c081d73930d15b
Danilo Pianini <danilo.pianini@unibo.it>
2022-07-08 00:21:32 +0200

    feat(release): use publish-on-central 2.x.x (#224)

--- release.config.js
+++ release.config.js
@@ -1,7 +1,7 @@
 var publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
+./gradlew uploadJava release --parallel || ./gradlew uploadJava release --parallel || ./gradlew uploadJava release --parallel || exit 1
 git push --force origin \${nextRelease.version}
-./gradlew releaseJavaMavenOnMavenCentralNexus --parallel || exit 1
 `
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
