83308b83c94a13fde6a9cc6e1f6c384ba1424e38
Danilo Pianini <danilo.pianini@unibo.it>
2023-02-21 16:31:40 +0100

    build: update the infrastructure with git hooks, gradle enterprise, and foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
+plugins {
+    id("com.gradle.enterprise") version "3.12.3"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.3"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
+
 rootProject.name = "java-quadtree"
-enableFeaturePreview("VERSION_CATALOGS")
