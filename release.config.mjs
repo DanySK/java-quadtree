a62b92e9bb6704d112baee109f9dc95427e4d448
Danilo Pianini <danilo.pianini@unibo.it>
2024-12-09 00:20:34 +0100

    ci(release): disambiguate the release task name

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,6 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
-./gradlew uploadJava release --parallel || ./gradlew uploadJava release --parallel || ./gradlew uploadJava release --parallel || exit 1
+./gradlew uploadJava releaseStagingRepositoryOnMavenCentral --parallel || exit 1
 git push --force origin \${nextRelease.version}
 `
 import config from 'semantic-release-preconfigured-conventional-commits' with { type: "json" };
